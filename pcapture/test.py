6aa1549c94e2a5047534db93ba31a77127d8bb7b
hoangbao1102 <hoangbao022002dn@gmail.com>
2024-11-06 00:39:03 +0700

    check mis match

--- pcapture/test.py
+++ pcapture/test.py
@@ -5,14 +5,18 @@ def check_header_mismatch(file1_path, file2_path):
     header1 = pd.read_csv(file1_path, nrows=0).columns.tolist()
     header2 = pd.read_csv(file2_path, nrows=0).columns.tolist()
 
+    # Normalize the headers by stripping whitespace and converting to lowercase
+    header1 = [col.strip().lower() for col in header1]
+    header2 = [col.strip().lower() for col in header2]
+
     # Iterate through headers and check for mismatches
     for column1, column2 in zip(header1, header2):
-        if column1==column2:
-            print(column1,' ',column2,'------------------------Eqaul')
+        if column1 == column2:
+            print(column1, ' ', column2, '------------------------Equal')
         else:
-            print(column1,' ',column2,'-----------Not Eqaul')
+            print(column1, ' ', column2, '-----------Not Equal')
 
 # Example usage:
-file1_path = "./packet_features.csv"
-file2_path = "../ref.csv"
+file1_path = "C:/DUT/Nam5/Ky9/ATTT/Cuoi Ky/packet_features.csv"
+file2_path = "C:/DUT/Nam5/Ky9/ATTT/dataset/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"
 check_header_mismatch(file1_path, file2_path)
